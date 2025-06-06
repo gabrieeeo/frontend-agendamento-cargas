6ea1a5cf9dcc3eb97d0f88e526614a6da7af0804
Gabriel <gabrieeelima15@gmail.com>
2025-06-05 22:22:21 -0300

    feat: adicionado metodo de cadastro no banco de dados

---
+++ src/components/fornecedor/cadastro.js
@@ -0,0 +1,24 @@
+const btnCadastrar = document.getElementById('btn-cadastrar');
+const nomeFornecedor = document.getElementById('nome-fornecedor');
+const cnpjFornecedor = document.getElementById('cnpj-fornecedor');
+
+btnCadastrar.addEventListener('click', async () => {
+    fetch('http://localhost:8080/fornecedores', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nomeFornecedor.value,
+            cnpj: cnpjFornecedor.value
+        })
+}).then(response => {
+    if (!response.ok) throw new Error('Erro ao fazer a requisição!');
+    return response.json();
+}).then(data => {
+    alert('Fornecedor cadastrado com sucesso!');
+}).catch(error => {
+    alert('Erro ao cadastrar fornecedor: ' + error.message);
+    console.error('Erro:', error);
+});
+});
